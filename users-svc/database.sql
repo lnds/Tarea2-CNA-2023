ab8200aeadc28f8b4530ba368b8b6c41518cbb83
Eduardo Diaz <ediaz@dcc8090.cl>
2023-09-10 20:00:26 -0300

    add rest service for auth

---
+++ users-svc/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    birth_day DATE NOT NULL
+)
+
